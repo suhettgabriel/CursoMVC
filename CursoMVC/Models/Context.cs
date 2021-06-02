38e2ee9ae502bd25fb4035b5a6820cfe5e8900ba
suhettgabriel <suhettgabriel1983@gmail.com>
2021-06-01 23:13:11 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrate Security=true");
+        }
+    }
+}
